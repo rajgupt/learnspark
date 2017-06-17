f922c23ccd93ab855fb6fd42e480f03cc3db5739
rajat.y6_gmail <rajat.y6_gmail@ip-172-31-100-98.ec2.internal>
2017-06-17 09:00:52 +0000

    first example

---
+++ ex1.py
@@ -0,0 +1,9 @@
+# existing collection in python 
+data = [1, 2, 3, 4, 5]
+
+# converted to RDD by parallelized
+distData = sc.parallelize(data)
+
+# find the sum by reduce
+distData.reduce(lambda a,b:a+b)
+
